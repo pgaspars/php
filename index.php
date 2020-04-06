74b1b028a11032b1663f8d7c4a37fe14603dbb5f
Pedro Gaspar <43708181+pgaspars@users.noreply.github.com>
2020-04-06 11:51:45 -0300

    limpeza de código

--- index.php
+++ index.php
@@ -2,11 +2,4 @@
 <?php
     echo "Olá Mundo!"
 ?>
-=======
-<?php 
-    echo "Inhaí";
-    echo "fdeo!!!";
-    echo "92";
-    echo "sacou?";
-?>
->>>>>>> 2b2138e7467b68d6f81f98d9ea278ffbb2771a15
+
